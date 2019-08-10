Fix geophysical input orientation; add simulated streamflow loading; add upscaling; add geophysical data scaling

src/datasets.py
@@ -101,19 +101,36 @@ class StatefulBatchSampler(torch.utils.data.Sampler):
 
     
 class RdrsGridDataset(Dataset):
-    """ RDRS dataset where target is a spatial grid of streamflow values """
-    def __init__(self, rdrs_vars, seq_len, seq_steps, date_start, date_end, conv_scalers=None, exclude_stations=[], aggregate_daily=None, include_months=False):
+    """ 
+    RDRS dataset where target is a spatial grid of streamflow values.
+    If include_simulated_streamflow, the returned items will be dicts y -> actual gauge stations, y_sim -> simulated gauges.
+    Else, the dict will only contain y.
+    
+    If out_lats and out_lons are not specified, will use RDRS grid as output resolution.
+    Specifying out_lats/lons allows for different resolution of geophysical (non-time-series) inputs.
+    """
+    def __init__(self, rdrs_vars, seq_len, seq_steps, date_start, date_end, conv_scalers=None, exclude_stations=[], aggregate_daily=None, include_months=False, include_simulated_streamflow=False, out_lats=None, out_lons=None):
         self.date_start = date_start
         self.date_end = date_end
         self.seq_len = seq_len
         self.seq_steps = seq_steps
         self.conv_scalers = conv_scalers
+        self.include_simulated_streamflow = include_simulated_streamflow
         
         rdrs_data, rdrs_var_names, rdrs_time_index, lats, lons = load_data.load_rdrs_forcings(as_grid=True, include_lat_lon=True)
         rdrs_data = rdrs_data[:,rdrs_vars,:,:]
         self.n_conv_vars = len(rdrs_vars)
         self.conv_height = rdrs_data.shape[2]
         self.conv_width = rdrs_data.shape[3]
+        
+        self.in_lats = lats
+        self.in_lons = lons
+        self.out_lats = self.in_lats if out_lats is None else np.tile(out_lats,len(out_lons)).reshape(len(out_lons),-1).T
+        self.out_lons = self.in_lons if out_lons is None else np.tile(out_lons,len(out_lats)).reshape(len(out_lats),-1)
+        if out_lats is not None and out_lons is not None:
+            # Create upsampling map so we can quickly upsample during training/testing.
+            self.upsample_map_rows, self.upsample_map_cols = map_to_geophysical_coords(self.in_lats, self.in_lons, 
+                                                                                       self.out_lats, self.out_lons):
         
         if aggregate_daily is not None:
             self.n_conv_vars += sum(1 for agg in aggregate_daily if agg == 'minmax')
@@ -155,9 +172,9 @@ class RdrsGridDataset(Dataset):
             station_lat = data_runoff[data_runoff['station'] == station]['Lat'].values[0]
             station_lon = data_runoff[data_runoff['station'] == station]['Lon'].values[0]
             # find nearest cell
-            station_idx = np.argmin(np.square(lats - station_lat) + np.square(lons - station_lon))
-            station_row = station_idx // rdrs_data.shape[3]
-            station_col = station_idx % rdrs_data.shape[3]
+            station_idx = np.argmin(np.square(self.out_lats - station_lat) + np.square(self.out_lons - station_lon))
+            station_row = station_idx // self.out_lons.shape[1]
+            station_col = station_idx % self.out_lons.shape[1]
             self.station_to_index[station] = (station_row, station_col)
         
         if include_months:
@@ -192,10 +209,25 @@ class RdrsGridDataset(Dataset):
             self.x_conv[:,:,i,:,:] = np.nan_to_num(self.conv_scalers[i].fit_transform(self.x_conv[:,:,i,:,:].reshape((-1, 1)))
                                                    .reshape(self.x_conv[:,:,i,:,:].shape))
 
+        if include_simulated_streamflow:
+            self.simulated_streamflow, self.outlet_to_row_col = load_data.load_simulated_streamflow()
+            # Create a tensor of shape (#days, height, width) of target values (only the cells of virtual gauges get populated)
+            self.y_sim = torch.zeros((len(self.dates), self.out_lats.shape[0], self.out_lats.shape[1]))
+             # Mask is True iff this cell is a subbasin's outlet
+            self.mask_sim = torch.zeros((self.out_lats.shape[0], self.out_lats.shape[1]), dtype=torch.bool)
+            for subbasin in self.simulated_streamflow['subbasin'].unique():
+                row, col = self.outlet_to_row_col[subbasin]
+                self.mask_sim[row, col] = True
+                subbasin_streamflow = self.simulated_streamflow[self.simulated_streamflow['subbasin']==subbasin].set_index('date')
+                for i in range(len(self.dates)):
+                    if self.dates[i] in subbasin_streamflow.index:
+                        self.y_sim[i, row, col] = subbasin_streamflow.loc[self.dates[i], 'simulated_streamflow']
+            
         self.x_conv = torch.from_numpy(self.x_conv).float()
         # Create a tensor of shape (#days, height, width) of target values (only those cells where we have stations get populated)
-        self.y = torch.zeros((len(self.dates), self.conv_height, self.conv_width))
-        self.mask = torch.zeros((len(self.dates), self.conv_height, self.conv_width), dtype=torch.bool)
+        self.y = torch.zeros((len(self.dates), self.out_lats.shape[0], self.out_lats.shape[1]))
+        # Mask is True iff we have an actual streamflow value for this cell at this date
+        self.mask = torch.zeros((len(self.dates), self.out_lats.shape[0], self.out_lats.shape[1]), dtype=torch.bool)
         for station in data_runoff['station'].unique():
             row, col = self.station_to_index[station]
             station_runoff = data_runoff[data_runoff['station']==station].set_index('date')
@@ -203,9 +235,13 @@ class RdrsGridDataset(Dataset):
                 if self.dates[i] in station_runoff.index:
                     self.mask[i, row, col] = True
                     self.y[i, row, col] = station_runoff.loc[self.dates[i], 'runoff']
-        
+                    
     def __getitem__(self, index):
-        return {'x_conv': self.x_conv[index,:,:,:,:], 'y': self.y[index], 'mask': self.mask[index]}
+        if self.include_simulated_streamflow:
+            return {'x_conv': self.x_conv[index,:,:,:,:], 'y': self.y[index], 'mask': self.mask[index],
+                    'y_sim': self.y_sim[index], 'mask_sim': self.mask_sim}
+        else:
+            return {'x_conv': self.x_conv[index,:,:,:,:], 'y': self.y[index], 'mask': self.mask[index]}
     
     def __len__(self):
         return self.y.shape[0]
@@ -213,10 +249,12 @@ class RdrsGridDataset(Dataset):
     
 class GeophysicalGridDataset(IterableDataset):
     """ Dataset of geophysical gridded inputs. """
-    def __init__(self, dem=True, landcover=True, soil=True, groundwater=True, min_lat=None, max_lat=None, min_lon=None, max_lon=None, landcover_types=None):
+    def __init__(self, dem=True, landcover=True, soil=True, groundwater=True, min_lat=None, max_lat=None, min_lon=None, max_lon=None, landcover_types=None, scalers=None):
 
+        self.scalers = scalers
+        
         min_lat_idx, max_lat_idx, min_lon_idx, max_lon_idx = load_data.get_bounding_box_indices(min_lat, max_lat, min_lon, max_lon)
-        self.item = np.zeros((0,max_lat_idx - min_lat_idx,max_lon_idx - min_lon_idx))
+        self.item = np.zeros((0,min_lat_idx - max_lat_idx,max_lon_idx - min_lon_idx))
         
         if dem:
             self.dem = load_data.load_dem(min_lat, max_lat, min_lon, max_lon)
@@ -230,9 +268,39 @@ class GeophysicalGridDataset(IterableDataset):
         if groundwater:
             self.groundwater = load_data.load_groundwater(min_lat, max_lat, min_lon, max_lon)
             self.item = np.concatenate([self.item, self.groundwater.reshape((1, self.groundwater.shape[0], self.groundwater.shape[1]))], axis=0)
-        
+            
+        # Scale training data
+        if self.scalers is None:
+            self.scalers = list(preprocessing.StandardScaler() for _ in range(self.item.shape[0]))
+        for i in range(self.item.shape[0]):
+            self.item[i,:,:] = np.nan_to_num(self.scalers[i].fit_transform(self.item[i,:,:].reshape((-1, 1))).reshape(self.item[i,:,:].shape))
+            
+        lats, lons = load_data.load_dem_lats_lons()
+        self.lats = np.tile(lats,len(lons)).reshape(len(lons),-1).T[max_lat_idx:min_lat_idx,min_lon_idx:max_lon_idx]
+        self.lons = np.tile(lons,len(lats)).reshape(len(lats),-1)[max_lat_idx:min_lat_idx,min_lon_idx:max_lon_idx]
         
     def __iter__(self):
         while True:
             yield self.item
+
+
+def map_to_geophysical_coords(lowres_lats, lowres_lons, geophys_lats, geophys_lons):
+    """
+    Generates a mapping from high-resolution cell to (row, col) in low-resolution grid
+    """
+    upsample_map_rows = np.zeros(geophys_lats.shape, dtype=int)
+    upsample_map_cols = np.zeros(geophys_lats.shape, dtype=int)
+    for i in range(geophys_lats.shape[0]):
+        for j in range(geophys_lats.shape[1]):
+            highres_lat, highres_lon = geophys_lats[i,j], geophys_lons[i,j]
+            closest_lowres_cell = np.argmin(np.square(lowres_lats - highres_lat) + np.square(lowres_lons - highres_lon))
+            upsample_map_rows[i,j] = closest_lowres_cell // lowres_lats.shape[1]
+            upsample_map_cols[i,j] = closest_lowres_cell % lowres_lats.shape[1]
     
+    return upsample_map_rows, upsample_map_cols
+
+def upsample_to_geophysical_input(lowres_data, upsample_map_rows, upsample_map_cols):
+    """
+    Upsamples lowres_data based on upsample_map
+    """
+    return lowres_data[upsample_map_rows, upsample_map_cols]

src/load_data.py
@@ -27,6 +27,35 @@ def load_discharge_gr4j_vic():
     return data_runoff
 
 
+def load_simulated_streamflow():
+    """
+    Loads VIC-GRU+Raven streamflow simulation for all subbasins.
+    Returns (DataFrame of simulations for each subbasin, dict subbasin -> (row, col) in the grid)
+    """
+    simulation_per_subbasin = pd.read_csv('../data/GRIP-E_Hydrographs_VIC-GRU_test2013+2014_allSubbasins.csv').drop(['time', 'hour', 'precip [mm/day]'], axis=1)
+
+    subbasin_nc = nc.Dataset('../data/simulations_shervan/Raven_input.nc', 'r')
+    subbasin_lat_lons = pd.DataFrame({'lat': subbasin_nc['lat'][:], 'lon': subbasin_nc['lon'][:]}, index=range(1,len(subbasin_nc['lat'][:])+1))
+    subbasin_nc.close()
+
+    simulation_per_subbasin = simulation_per_subbasin.rename(lambda c: int(c[3:].replace(' [m3/s]', '')) if c not in ['date', 'hour', 'precip [mm/day]'] else c, axis=1)
+    simulation_per_subbasin = simulation_per_subbasin.set_index('date').transpose().unstack().reset_index().rename({'level_0': 'date', 'level_1': 'subbasin', 0: 'simulated_streamflow'}, axis=1)
+    simulation_per_subbasin['date'] = pd.to_datetime(simulation_per_subbasin['date'])
+    simulation_per_subbasin['date'] = simulation_per_subbasin['date'] - timedelta(days=1)  # Need to lag raven output back by one day.
+    simulation_per_subbasin = simulation_per_subbasin.join(subbasin_lat_lons, on='subbasin')
+
+    lats, lons = load_dem_lats_lons()
+    subbasin_outlet_to_index = {}
+    for subbasin in simulation_per_subbasin['subbasin'].unique():
+        outlet_lat, outlet_lon = subbasin_lat_lons.loc[subbasin, ['lat', 'lon']]
+        # find nearest cell
+        outlet_row = np.argmin(np.square(lats - outlet_lat))
+        outlet_col = np.argmin(np.square(lons - outlet_lon))
+        subbasin_outlet_to_index[subbasin] = (outlet_row, outlet_col)
+    
+    return simulation_per_subbasin, subbasin_outlet_to_index
+
+    
 def load_rdrs_forcings(as_grid=False, include_lat_lon=False):
     """
     Loads RDRS gridded forcings from disk. 
@@ -317,9 +346,9 @@ def load_landcover(values_to_use=None, min_lat=None, max_lat=None, min_lon=None,
     
     if not os.path.isfile(filename):
         import gdal
-        dem_nc = nc.Dataset('../data/geophysical/dem/hydrosheds_n40-45w75-90_30sec.nc', 'r')
         landcover_nc = nc.Dataset('../data/geophysical/landcover/NA_NALCMS_LC_30m_LAEA_mmu12_urb05_n40-45w75-90.nc', 'r')
         landcover = landcover_nc['Band1'][:].filled(np.nan)
+        dem_lats, dem_lons = load_dem_lats_lons()
         
         landcover_30sec_nc = nc.Dataset('../data/geophysical/landcover/NA_NALCMS_LC_30m_LAEA_mmu12_urb05_n40-45w75-90_30sec.nc', 'w')
         landcover_30sec_nc.setncattr('Conventions', 'CF-1.6')
@@ -330,14 +359,12 @@ def load_landcover(values_to_use=None, min_lat=None, max_lat=None, min_lon=None,
             landcover_30sec_nc['crs'].setncattr(attr, landcover_nc['crs'].getncattr(attr))
         landcover_30sec_nc.createVariable('lat', np.float64, dimensions=('lat'))
         landcover_30sec_nc.createVariable('lon', np.float64, dimensions=('lon'))
-        landcover_30sec_nc['lat'][:] = dem_nc['lat'][:]
-        landcover_30sec_nc['lon'][:] = dem_nc['lon'][:]
+        landcover_30sec_nc['lat'][:] = dem_lats
+        landcover_30sec_nc['lon'][:] = dem_lons
         for attr in landcover_nc['lat'].ncattrs():
             landcover_30sec_nc['lat'].setncattr(attr, landcover_nc['lat'].getncattr(attr))
         for attr in landcover_nc['lon'].ncattrs():
             landcover_30sec_nc['lon'].setncattr(attr, landcover_nc['lon'].getncattr(attr))
-        
-        dem_nc.close()
         
         # gdal.Warp can only resample one band at a time. Hence, resample each landtype separately and successively merge into _30sec.nc.
         for i, landtype in landcover_legend.items():
@@ -388,7 +415,7 @@ def load_landcover(values_to_use=None, min_lat=None, max_lat=None, min_lon=None,
     landcover = np.zeros((0, landcover_nc['lat'].shape[0], landcover_nc['lon'].shape[0]))
     legend = []
     for i in range(len(values_to_use)):
-        landcover_i = landcover_nc['landtype_{}'.format(values_to_use[i])][:].filled(np.nan)
+        landcover_i = landcover_nc['landtype_{}'.format(values_to_use[i])][:][::-1,:].filled(np.nan)
         if landcover_i.sum() == 0:
             continue
         landcover = np.concatenate([landcover, landcover_i.reshape((1,landcover.shape[1],landcover.shape[2]))], axis=0)
@@ -396,7 +423,7 @@ def load_landcover(values_to_use=None, min_lat=None, max_lat=None, min_lon=None,
     
     landcover_nc.close()
     min_lat_idx, max_lat_idx, min_lon_idx, max_lon_idx = get_bounding_box_indices(min_lat, max_lat, min_lon, max_lon)
-    return landcover[:,min_lat_idx:max_lat_idx,min_lon_idx:max_lon_idx], legend
+    return landcover[:,max_lat_idx:min_lat_idx,min_lon_idx:max_lon_idx], legend
 
 
 def load_dem(min_lat=None, max_lat=None, min_lon=None, max_lon=None):
@@ -405,10 +432,10 @@ def load_dem(min_lat=None, max_lat=None, min_lon=None, max_lon=None):
     If min/max lat/lon is specified, will only return the specified sub-area.
     """
     dem_nc = nc.Dataset('../data/geophysical/dem/hydrosheds_n40-45w75-90_30sec.nc', 'r')
-    dem = dem_nc['Band1'][:].filled(np.nan)
+    dem = dem_nc['Band1'][:][::-1,:].filled(np.nan)
 
     min_lat_idx, max_lat_idx, min_lon_idx, max_lon_idx = get_bounding_box_indices(min_lat, max_lat, min_lon, max_lon)
-    return dem[min_lat_idx:max_lat_idx,min_lon_idx:max_lon_idx]
+    return dem[max_lat_idx:min_lat_idx,min_lon_idx:max_lon_idx]
 
 def load_groundwater(min_lat=None, max_lat=None, min_lon=None, max_lon=None):
     """
@@ -416,10 +443,10 @@ def load_groundwater(min_lat=None, max_lat=None, min_lon=None, max_lon=None):
     If min/max lat/lon is specified, will only return the specified sub-area.
     """
     groundwater_nc = nc.Dataset('../data/geophysical/groundwater/N_America_model_wtd_v2_n40-45w75-90.nc', 'r')
-    groundwater = groundwater_nc['Band1'][:].filled(np.nan)
+    groundwater = groundwater_nc['Band1'][:][::-1,:].filled(np.nan)
     
     min_lat_idx, max_lat_idx, min_lon_idx, max_lon_idx = get_bounding_box_indices(min_lat, max_lat, min_lon, max_lon)
-    return groundwater[min_lat_idx:max_lat_idx,min_lon_idx:max_lon_idx]
+    return groundwater[max_lat_idx:min_lat_idx,min_lon_idx:max_lon_idx]
 
 def load_soil(min_lat=None, max_lat=None, min_lon=None, max_lon=None):
     """
@@ -437,21 +464,35 @@ def load_soil(min_lat=None, max_lat=None, min_lon=None, max_lon=None):
         for j in [1,2]:
             soil_nc = nc.Dataset('../data/geophysical/soil/{}{}_n40-45w75-90.nc'.format(soiltypes[i], j), 'r')
             for layer in range(1,5):
-                soil[i*8 + ((j-1)*4 + layer-1)] = soil_nc['Band{}'.format(layer)][:].astype(np.float).filled(np.nan) / 100.0
+                soil[i*8 + ((j-1)*4 + layer-1)] = soil_nc['Band{}'.format(layer)][:][::-1,:]\
+                        .astype(np.float).filled(np.nan) / 100.0
                 soil_legend.append('{}-layer{}'.format(soiltypes[i], (j-1)*4 + layer))
             soil_nc.close()
     
     min_lat_idx, max_lat_idx, min_lon_idx, max_lon_idx = get_bounding_box_indices(min_lat, max_lat, min_lon, max_lon)
-    return soil[:,min_lat_idx:max_lat_idx,min_lon_idx:max_lon_idx], soil_legend
+    return soil[:,max_lat_idx:min_lat_idx,min_lon_idx:max_lon_idx], soil_legend
+
+
+def load_dem_lats_lons():
+    """
+    Returns arrays of lats and lons of 30" DEM dataset.
+    Note that lats are decreasing with index.
+    """
+    dem_nc = nc.Dataset('../data/geophysical/dem/hydrosheds_n40-45w75-90_30sec.nc', 'r')
+    lats = dem_nc['lat'][:][::-1].filled(np.nan)
+    lons = dem_nc['lon'][:].filled(np.nan)
+    dem_nc.close()
+    
+    return lats, lons
 
 
 def get_bounding_box_indices(min_lat, max_lat, min_lon, max_lon):
     """
     Returns indices to split the 30" datasets such that they only contain lats/lons within the specified bounding box.
+    Note that min_lat_idx will be larger than max_lat_idx, because latitudes decrease with index.
     """
-    dem_nc = nc.Dataset('../data/geophysical/dem/hydrosheds_n40-45w75-90_30sec.nc', 'r')
-    lats = dem_nc['lat'][:]
-    lons = dem_nc['lon'][:]
+    lats, lons = load_dem_lats_lons()
+    lats = lats[::-1]  # lats go from high to low
     
     if min_lat is None:
         min_lat = lats.min()
@@ -465,11 +506,10 @@ def get_bounding_box_indices(min_lat, max_lat, min_lon, max_lon):
     if min_lat > lats.max() or max_lat < lats.min() or min_lon > lons.max() or max_lon < lons.min():
         raise Exception('Empty lat/lon selection.')
 
-    min_lat_idx = (lats >= min_lat).argmax()
-    max_lat_idx = (lats <= max_lat).argmin() if max_lat < lats.max() else len(lats)
+    min_lat_idx = len(lats) - (lats >= min_lat).argmax() - 1
+    max_lat_idx = len(lats) - (lats >= max_lat).argmax() - 1 if max_lat <= lats.max() else 0
     min_lon_idx = (lons >= min_lon).argmax()
-    max_lon_idx = (lons <= max_lon).argmin() if max_lon < lons.max() else len(lons)
-    dem_nc.close()
+    max_lon_idx = (lons >= max_lon).argmax() if max_lon < lons.max() else len(lons)
     
     return min_lat_idx, max_lat_idx, min_lon_idx, max_lon_idx
     